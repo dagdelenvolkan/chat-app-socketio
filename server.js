36f82b40378291862c9a002643e379e506f923f2
dagdelenvolkan <dagdelen16@itu.edu.tr>
2021-07-14 14:43:28 +0300

    Add login page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const http    = require('http')
-const { Server }      = require('socket.io')
+const express    = require('express')
+const http       = require('http')
+const { Server } = require('socket.io')
 
 const app    = express()
 const server = http.createServer(app)
